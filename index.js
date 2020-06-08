173453fef6132118b05024a4d0cca1159df6ea5d
pranav0281999 <pranav0281999@gmail.com>
2020-06-08 15:59:58 +0530

    Adds chat router

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const express = require('express');
+const cors = require('cors');
+const chatRouter = require('./routes/chat');
+const app = express();
+
+require('dotenv').config();
+
+app.use(cors());
+const port = process.env.PORT || 5000;
+app.use(express.json());
+
+app.use('/chat', chatRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
