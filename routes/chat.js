3aa0b335db4556cb9a0179d1ad90cc654733cc47
pranav0281999 <pranav0281999@gmail.com>
2020-06-11 02:17:10 +0530

    Adds /chat/video get post

--- routes/chat.js
+++ routes/chat.js
@@ -45,4 +45,47 @@ router.route('/image').get((req, res) => {
     res.sendFile(path.join(__dirname, "../public/image/" + req.query.filename));
 });
 
+var storageVideo = multer.diskStorage({
+    destination: './public/video',
+    filename: function (req, file, callback) {
+        callback(null, req.headers.filename + path.extname(file.originalname));
+    }
+});
+
+var uploadVideo = multer({
+    storage: storageVideo,
+    fileFilter: (req, file, callback) => {
+        checkVideoFileType(file, callback);
+    }
+}).single("video");
+
+checkVideoFileType = (file, callback) => {
+    const fileTypes = /mov|mpeg4|mp4|avi|wmv|flv|mpegps|webm|/;
+    const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+
+    if (extName) {
+        callback(null, true);
+    } else {
+        callback("Error: video only");
+    }
+}
+
+router.route('/video').post((req, res) => {
+    console.log(req.headers);
+
+    uploadVideo(req, res, (err) => {
+        if (err) {
+            res.status(400).json({
+                msg: err
+            })
+        } else {
+            res.status(200).json({ msg: "video uploaded" })
+        }
+    });
+});
+
+router.route('/video').get((req, res) => {
+    res.sendFile(path.join(__dirname, "../public/video/" + req.query.filename));
+});
+
 module.exports = router;
